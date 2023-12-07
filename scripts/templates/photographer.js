05f701651acf05a1ecc2a94b7dc99ac8674b0520
oliverfont <oliverfont@protonmail.com>
2023-12-07 16:34:36 +0100

    Integration JS dans DOM des photos et infos JSON

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,37 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { id, name, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/PhotographersID/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute('src', picture);
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+
+        const pId = document.createElement('p');
+        pId.textContent = `ID: ${id}`;
+
+        const pTagline = document.createElement('p');
+        pTagline.textContent = tagline;
+
+        const pLocation = document.createElement('p');
+        pLocation.textContent = `${city}, ${country}`;
+
+        const pPrice = document.createElement('p');
+        pPrice.textContent = `Starting at $${price}`;
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(pId);
+        article.appendChild(pTagline);
+        article.appendChild(pLocation);
+        article.appendChild(pPrice);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { id, name, picture, getUserCardDOM };
 }
